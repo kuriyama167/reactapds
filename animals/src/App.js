3dc59dc64c9a19ecd3bca04a7608536a17bc2914
Isabel Velasquez <velasquezclara167@gmail.com>
2024-05-29 15:29:56 -0600

    sates

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,15 +1,21 @@
+import {useState} from 'react';
 function App(){
   //  const handleClick = ()=> {
     //    console.log('Button Was clicked!')
-
     //};
+    const [count, setCount]= useState(0);
+
+    const handleClick=()=>{
+        setCount(count+1);
+    };
+    
     return <div>
-       <button
-        onClick={()=> 
-            console.log('button was clicked!')
-        }>
+       <button onClick={handleClick}>
             Add Animal
         </button>
+        <div>Number of animals: {count}</div>
+
+
     </div>;
 }
 export default App;
