d57ede23e9cfedc92f6c824632ca56a5c23aaf61
Isabel Velasquez <velasquezclara167@gmail.com>
2025-05-26 00:12:58 -0600

    axios and json server port fixed

--- books/src/App.js
+++ books/src/App.js
@@ -4,55 +4,43 @@ import BookList from "./components/BookList";
 import BookShow from "./components/BookShow";
 import axios from "axios";
 
-function App (){
+function App() {
+  const [books, setBooks] = useState([]);
 
-    const [books, setBooks]=useState([]); 
+  const editBookId = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const handleCreateBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
    
-     const editBookId=(id, newTitle) => {
-        const updatedBooks = books.map((book)=>{
-             if(book.id === id){
-                return {...book, title:newTitle};
-             }
-             return book;
-        });
-        setBooks(updatedBooks);
-     }
 
-    const deleteBookById=(id)=>{
-        const updatedBooks=books.filter((book)=>{
-            return book.id!==id;
-        });
-        setBooks(updatedBooks);
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
+  };
 
-    };
-
-
-
-    const handleCreateBook = async (title) => {
-      const response = await axios.post("http://localhost:3000/books", {
-        title
-      });
-      console.log(response); 
-
-      // const updatedBooks=[
-      //   ...books,
-      //   {
-      //      id:Math.round(Math.random()*9999),
-      ////       title:title
-      //   }
-      //  ];
-      // setBooks(updatedBooks);
-    };
-
-    return (
-        <div className="app">
-            <h1>Reading List</h1>
-        <BookList onEdit={editBookId} books={books} onDelete={deleteBookById} />    
-        <BookCreate onCreate={handleCreateBook}/>
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList onEdit={editBookId} books={books} onDelete={deleteBookById} />
+      <BookCreate onCreate={handleCreateBook} />
     </div>
-    );
-        
+  );
 }
 
 export default App;
