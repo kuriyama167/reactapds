211abcde00872c80752f5e61b460703fb9beed79
Isabel Velasquez <velasquezclara167@gmail.com>
2025-05-27 21:53:46 -0600

    usecontext

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,11 +1,19 @@
 import BookShow from "./BookShow"
+import { useContext } from "react";
+import BooksContext from "../context/book";
+
 
 function BookList({books, onDelete , onEdit}){
+      const {count, incrementCount} = useContext(BooksContext)
+
 
     const rederedBooks = books.map((book)=> {
  return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>;
     });
-    return <div className="book-list"> {rederedBooks}</div>;
+    return <div className="book-list"> 
+    {count}
+    <button onClick={incrementCount}>Click</button>
+    {rederedBooks}</div>;
 }
 
 export default BookList;
